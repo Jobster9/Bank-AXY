146110fe828034f7f6752eaecadd05a74041402d
Joby Else <113983712+Jobster9@users.noreply.github.com>
2023-03-09 11:07:25 +0000

    completed stage 2
    function created & tested

--- database-connection.php
+++ database-connection.php
@@ -8,52 +8,51 @@
 <!-- Stage 2 -->
 <?php
 
-$checkFunction = insertToDatabase();
-
-insertToDatabase(){
-
-    $success = true;
+$result = insertToDatabase();
+function insertToDatabase()
+{
+    $status = true;
     // Set up connection parameters
     $serverName = "DESKTOP-AVCNUEK"; // Change to your server name
     $connectionInfo = array(
-    "Database" => "testdatabase",
+        "Database" => "testdatabase",
     );
-
     // Connect to database
     $conn = sqlsrv_connect($serverName, $connectionInfo);
-
     if ($conn === false) {
         // Handle connection error
-        die(print_r(sqlsrv_errors(), true));
+        print_r(sqlsrv_errors());
+        $status = false;
     }
-
     // Define SQL statement for inserting record
     $sql = "INSERT INTO mytable (mycolumn) VALUES (?)";
-
     // Prepare the SQL statement
     $stmt = sqlsrv_prepare($conn, $sql, array("successful value added"));
-
     if ($stmt === false) {
         // Handle statement preparation error
-        die(print_r(sqlsrv_errors(), true));
-        $success = false;
+        print_r(sqlsrv_errors());
+        $status = false;
     }
-
     // Execute the prepared statement
     if (sqlsrv_execute($stmt) === false) {
         // Handle statement execution error
-        die(print_r(sqlsrv_errors(), true));
-        $success = false;
+        print_r(sqlsrv_errors());
+        $status = false;
     }
-
     // Close the statement and connection
     sqlsrv_free_stmt($stmt);
     sqlsrv_close($conn);
-    return success;
+    return $status;
 }
 ?>
-
-
 <body>
-<h1><?php if($checkFunction){ echo ("successful insertion");}else{echo ("insertion failed");} ?> </h1>
+<h1><?php if ($result) {
+    echo ("successful insertion");
+} else {
+    echo ("insertion failed");
+    if (!is_null(sqlsrv_errors())) {
+        echo (print_r(sqlsrv_errors()));
+    }
+    ;
+} ?> </h1>
 </body>
