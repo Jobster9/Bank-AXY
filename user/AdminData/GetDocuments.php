92407846c7033aec08aa16a857a1ec227bec8f7c
AbdulRaheemNazir <c2010722@hallam.shu.ac.uk>
2023-03-17 14:36:43 +0000

    1
    1

--- user/AdminData/GetDocuments.php
+++ user/AdminData/GetDocuments.php
@@ -3,12 +3,7 @@
 function getUsers (){
 
     // Specify your database credentials here
-    $host = 'ABDULNAZIR';
-    $dbname = 'BankAXY';
-
-
-    // Create a new PDO connection object
-    $pdo = new PDO("sqlsrv:Server=$host;Database=$dbname");
+    include("../../DB config.php");
 
     $stmt = $pdo->prepare('SELECT * FROM Documents');
 
