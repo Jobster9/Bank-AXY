3c23b4608bded3b15f94988b2da5cdbdc189e407
JoeHoughton2003 <joehoughton92@gmail.com>
2023-03-17 17:46:02 +0000

    changes to the dashboard

---
+++ user/StaffData/GetChartDocs.php
@@ -0,0 +1,19 @@
+<?php
+
+function getChartDocs (){
+
+    // Specify your database credentials here
+    include("../DB config.php");
+
+    $stmt = $pdo->prepare('SELECT * FROM Documents WHERE Owner_ID = :User_ID');
+    $stmt->bindParam(':User_ID', $_SESSION['User_ID'], PDO::PARAM_STR);
+    $result = $stmt->execute();
+
+    $rows_array = [];
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $rows_array[] = $row;
+    }
+
+    return $rows_array;
+}
+
