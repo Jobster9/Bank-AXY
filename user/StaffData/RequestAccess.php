d0703eba6d0a5b99e7c096f369b219a2781abf7e
EmilyBaxter <emily.g.baxter@btinternet.com>
2023-03-19 20:48:07 +0000

    Updated page names and icons

--- user/StaffData/RequestAccess.php
+++ user/StaffData/RequestAccess.php
@@ -16,7 +16,7 @@ include("GetDocuments.php");
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<title>Transfer</title>
+<title>Access Request</title>
 
 <!-- Favicons -->
 <link href="../../assets/img/favicon-32x32.png" rel="icon">
