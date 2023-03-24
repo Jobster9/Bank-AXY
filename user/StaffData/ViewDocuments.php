b5856cfb06c053a401fd36cc50f3c4c8d13fd855
DugganDuggan <dwdafsg@gmail.com>
2023-03-24 14:57:12 +0000

    Fixed View document search becaues i broke it

--- user/StaffData/ViewDocuments.php
+++ user/StaffData/ViewDocuments.php
@@ -412,7 +412,7 @@ document.addEventListener('contextmenu', event => event.preventDefault());
 			 		
 </div>
 </div>
-<table class="styled-table" id="table-id">
+<table class="styled-table" id="myTable">
     <thead>
     </div>
         <tr>
@@ -484,7 +484,7 @@ for ($i = 0; $i < count($user); $i++):
 </div> <!-- 		End of Container -->
 
 <script>
-             getPagination('#table-id');
+             getPagination('#myTable');
 					//getPagination('.table-class');
 					//getPagination('table');
 
@@ -703,7 +703,7 @@ $(function() {
             table = document.getElementById("myTable");
             tr = table.getElementsByTagName("tr");
             for (i = 0; i < tr.length; i++) {
-                td = tr[i].getElementsByTagName("td")[0];
+                td = tr[i].getElementsByTagName("td")[1];
                 if (td) {
                 txtValue = td.textContent || td.innerText;
                 if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -721,7 +721,7 @@ $(function() {
             table = document.getElementById("myTable");
             tr = table.getElementsByTagName("tr");
             for (i = 0; i < tr.length; i++) {
-                td = tr[i].getElementsByTagName("td")[1];
+                td = tr[i].getElementsByTagName("td")[2];
                 if (td) {
                 txtValue = td.textContent || td.innerText;
                 if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -739,7 +739,7 @@ $(function() {
             table = document.getElementById("myTable");
             tr = table.getElementsByTagName("tr");
             for (i = 0; i < tr.length; i++) {
-                td = tr[i].getElementsByTagName("td")[2];
+                td = tr[i].getElementsByTagName("td")[3];
                 if (td) {
                 txtValue = td.textContent || td.innerText;
                 if (txtValue.toUpperCase().indexOf(filter) > -1) {
